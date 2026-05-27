7e49f7733d19e12dabc8487e10c11da9ce36629c
Carl Eskång <ceskang@kth.se>
2026-05-27 10:54:17 +0200

    Interior design v2

--- src/NPCInteraction.cpp
+++ src/NPCInteraction.cpp
@@ -2,19 +2,21 @@
 
 NPCInteraction::NPCInteraction(const NPCInteractionDef& def)
     : def(def) {}
-
+//update interaction each frame
 void NPCInteraction::update(GLFWwindow* window, const glm::vec3& playerPosition) {
+    //ignore y space
     glm::vec2 playerXZ = glm::vec2(playerPosition.x, playerPosition.z);
     glm::vec2 npcXZ    = glm::vec2(def.position.x,   def.position.z);
 
+    //chack if player is near nps
     playerIsNear = glm::distance(playerXZ, npcXZ) < def.interactionDistance;
     bool ePressed = glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS;
 
-    // Toggle dialog open/closed on a fresh E press while nearby
+    //Toggle dialog open/closed if e is pressed
     if (playerIsNear && ePressed && !eWasPressed)
         interacted = !interacted;
 
-    // Auto-close when the player walks away
+    //auto-close when the player walks away
     if (!playerIsNear)
         interacted = false;
 
